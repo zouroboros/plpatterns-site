15e201fd4393b455790dc444952c458f3b1512d9
Zouroboros <zouroboros@zoho.eu>
2023-04-19 18:38:39 +0200

    Implemented searching through languages.

--- app/Server/Patterns.hs
+++ app/Server/Patterns.hs
@@ -5,20 +5,20 @@
 
 module Server.Patterns(app) where
 
-import Data.Text (Text)
+import Data.Text (Text, isInfixOf)
 import Data.Morpheus (App, deriveApp)
 import Data.Morpheus.Types (GQLType, RootResolver(queryResolver), Undefined, defaultRootResolver, ResolverQ, liftEither)
 import GHC.Generics (Generic)
 
 import ExampleDb(ExampleDb(categories, examplesByCategory), Example(Example, id, name, description, example))
 import FileBasedDb(createDb)
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, fromMaybe)
 
 data Language = Language { name :: Text, pattern :: [Pattern] } deriving (GQLType, Generic)
 
-data LanguageArgs = LanuageArgs { name :: Maybe Text } deriving (GQLType, Generic)
+data LanguageArgs = LanguageArgs { name :: Maybe Text } deriving (GQLType, Generic)
 
-data Pattern = Pattern { name :: Text, code :: Text, language :: Language } deriving (GQLType, Generic)
+data Pattern = Pattern { name :: Text, code :: Text, description :: Text, language :: Language } deriving (GQLType, Generic)
 
 data Query m = Query { languages :: LanguageArgs -> m [Language] } deriving (GQLType, Generic)
 
@@ -29,19 +29,20 @@ resolveLanguages :: LanguageArgs -> ResolverQ e IO [Language]
 resolveLanguages args = liftEither $ getLanguages args
 
 getLanguages :: LanguageArgs -> IO (Either String [Language])
-getLanguages args = do
-    cats <- categories db
-    languages <- mapM getLanguage cats
+getLanguages LanguageArgs { name = query } = do
+    allCategories <- categories db
+    let filteredCategories = filter (\categoryName -> maybe True (`isInfixOf` categoryName) query) allCategories
+    languages <- mapM getLanguage filteredCategories
     return $ Right $ catMaybes languages
 
 getLanguage :: Text -> IO (Maybe Language)
 getLanguage name = do
-   examples <- examplesByCategory db name
-   return $ fmap (\patterns -> let lang = Language { name = name, pattern = map (toPattern lang) patterns } in lang) examples
+    examples <- examplesByCategory db name
+    return $ fmap (\patterns -> let lang = Language { name = name, pattern = map (toPattern lang) patterns } in lang) examples
 
 toPattern :: Language -> Example FilePath Text -> Pattern
-toPattern language Example { name = name, description = description, example = example } = 
-    Pattern { name = name, code = description, language = language }
+toPattern language Example { name = name, description = description, example = example } =
+    Pattern { name = name, code = example, description = description, language = language }
 
 rootResolver :: RootResolver IO () Query Undefined Undefined
 rootResolver = defaultRootResolver { queryResolver = Query { languages = resolveLanguages } }
