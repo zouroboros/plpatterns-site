82e5825175d402c58957d27457cf2bb61f9f200b
Zouroboros <zouroboros@zoho.eu>
2023-04-28 16:09:45 +0200

    Made playground and allow origin configurable via dotenv files.

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,17 @@
 
 module Main (main) where
 
+import Configuration.Dotenv (loadFile, defaultConfig)
+import System.Environment (lookupEnv)
+import Data.Text.Lazy (pack)
+
 import Server.Scotty (scottyServer)
 
 main :: IO ()
-main = scottyServer True (Just "*")
+main = do
+    loadFile defaultConfig
+    enablePlaygroundVar <- lookupEnv "ENABLEPLAYGROUND"
+    allowOriginVar <- lookupEnv "ALLOWORIGIN"
+    let enablePlayground = maybe False (== "TRUE") enablePlaygroundVar
+    let allowOrigin = fmap pack allowOriginVar
+    scottyServer enablePlayground allowOrigin
